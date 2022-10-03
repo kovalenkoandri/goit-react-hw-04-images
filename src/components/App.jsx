58c7faa7a6b6426ab9699e6d0a9ff9c8625bb6bc
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-03 16:39:27 +0300

    componentDidMount response

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export class App extends Component {
+  state = {
+    articles: [],
+    input: '',
+  };
+  async componentDidMount() {
+    const response = await axios.get(`?q=${this.state.input}`, {
+      params: {
+        key: '29101880-694af7e9974b3c9bb9fbf3052',
+        image_typemit: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 3,
+        // page: this.page,
+      },
+    });
+    this.setState({ articles: response.data.hits });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      input: event.currentTarget.elements.input.value,
+    });
+    console.log(this.state.input);
+    console.log(event.currentTarget.elements.input.value);
+    event.currentTarget.elements.input.value = '';
+  };
+  render() {
+    const { articles } = this.state;
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            name="input"
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+        <ul>
+          {articles.length > 0 &&
+            articles.map(({ id, pageURL, tags }) => (
+              <li key={id}>
+                <a href={pageURL} target="_blank" rel="noreferrer noopener">
+                  {tags}
+                </a>
+              </li>
+            ))}
+        </ul>
+      </header>
+    );
+  }
+}
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,4 +1,4 @@
-<header class="searchbar">
+{/* <header class="searchbar">
   <form class="form">
     <button type="submit" class="button">
       <span class="button-label">Search</span>
@@ -12,26 +12,4 @@
       placeholder="Search images and photos"
     />
   </form>
-</header>;
-import React, { Component } from 'react';
-import axios from 'axios';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-class AxiosGetApiService {
-    gallery = document.querySelector('.gallery');
-    constructor() {
-        this.page = 1;
-        this.inputValue = '';
-    }
-    async axiosGet() {
-        return await axios.get(`?q=${this.inputValue}`, {
-            params: {
-                key: '29101880-694af7e9974b3c9bb9fbf3052',
-                image_typemit: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-                per_page: 30,
-                page: this.page,
-            },
-        });
-    }
-}
+</header>; */}
