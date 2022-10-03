ef71b516deb8e4f386d891c905ad9aea335c5168
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-03 23:55:09 +0300

    export class ImageGallery extends Component

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
-export const ImageGallery = articles => {
-  console.log(articles);
-  return (
-    <ul className="gallery">
-      {articles.length > 0 &&
-        articles.map(({ id, pageURL, tags }) => (
-          <li key={id}>
-            <a href={pageURL} target="_blank" rel="noreferrer noopener">
-              {tags}
-            </a>
-          </li>
-        ))}
-    </ul>
-  );
-};
+import React, { Component } from 'react';
+export class ImageGallery extends Component {
+  state = {
+    articles: [],
+    input: '',
+  };
+ componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, this.props.articles);
+    prevProps.articles !== this.props.articles &&
+      this.setState({ articles: this.props.articles });
+  }
+  render() {
+    return (
+      <ul className="gallery">
+        {this.state.articles.length > 0 &&
+          this.state.articles.map(({ id, pageURL, tags }) => (
+            <li key={id}>
+              <a href={pageURL} target="_blank" rel="noreferrer noopener">
+                {tags}
+              </a>
+            </li>
+          ))}
+      </ul>
+    );
+  }
+}
 /* <!-- Набор <li> с изображениями --> */
 ImageGallery.propTypes = {
   articles: PropTypes.arrayOf(PropTypes.object).isRequired,
