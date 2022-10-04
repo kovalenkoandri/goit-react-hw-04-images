46e3ce7582656eb2374f3b1a3bb043a0111da224
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-04 16:44:07 +0300

    try  response

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,18 +11,22 @@ export class App extends Component {
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    const response = await axios.get(`?q=${this.state.input}`, {
-      params: {
-        key: '29101880-694af7e9974b3c9bb9fbf3052',
-        image_typemit: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 3,
-        page: 1,
-      },
-    });
-    prevState.input !== this.state.input &&
-      this.setState({ articles: response.data.hits });
+    try {
+      const response = await axios.get(`?q=${this.state.input}`, {
+        params: {
+          key: '29101880-694af7e9974b3c9bb9fbf3052',
+          image_typemit: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          per_page: 3,
+          page: 1,
+        },
+      });
+      prevState.input !== this.state.input &&
+        this.setState({ articles: response.data.hits });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   handleSubmit = event => {
