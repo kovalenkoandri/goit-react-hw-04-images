46e219f2cce90fc8a57a83aa7828ad4af7c563b5
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-04 16:19:43 +0300

    ImageGalleryItem

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,50 +1,22 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { ImageGalleryItem } from './ImageGalleryItem';
 export class ImageGallery extends Component {
   state = {
     articles: [],
-    input: '',
   };
- componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     prevProps.articles !== this.props.articles &&
       this.setState({ articles: this.props.articles });
   }
   render() {
-    console.log(this.state.articles);
     return (
-      <ul className="gallery" style={{ display: 'block' }}>
-        {this.state.articles.length > 0 &&
-          this.state.articles.map(({ id, pageURL, tags, previewURL }) => (
-            <li key={id}>
-              <a
-                href={pageURL}
-                target="_blank"
-                rel="noreferrer noopener"
-                style={{ display: 'block' }}
-              >
-                <img
-                  src={previewURL}
-                  alt={tags}
-                  style={{
-                    margin: 8,
-                    padding: '12px 16px',
-                    borderRadius: 4,
-                    backgroundColor: 'gray',
-                    color: 'white',
-                    display: 'block',
-                    width: '100px',
-                    height: '100px',
-                  }}
-                />
-                {tags}
-              </a>
-            </li>
-          ))}
+      <ul className="gallery">
+        <ImageGalleryItem articles={this.state.articles} />
       </ul>
     );
   }
 }
-/* <!-- Набор <li> с изображениями --> */
 ImageGallery.propTypes = {
   articles: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
--- src/components/ImageGalleryItem.js
+++ src/components/ImageGalleryItem.js
@@ -1,3 +1,29 @@
-<li class="gallery-item">
-  <img src="" alt="" />
-</li>;
+import PropTypes from 'prop-types';
+export const ImageGalleryItem = ({ articles }) => {
+  return (
+    <>
+      {articles.length > 0 &&
+        articles.map(({ id, tags, previewURL }) => (
+          <li key={id} className="gallery-item">
+            <img
+              src={previewURL}
+              alt={tags}
+              style={{
+                margin: 8,
+                padding: '12px 16px',
+                borderRadius: 4,
+                backgroundColor: 'gray',
+                color: 'white',
+                display: 'block',
+                width: '100px',
+                height: '100px',
+              }}
+            />
+          </li>
+        ))}
+    </>
+  );
+};
+ImageGalleryItem.propTypes = {
+  articles: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
