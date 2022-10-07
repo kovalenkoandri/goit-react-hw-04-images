d70417caa26bdd2c4b46284c1983e205767497d0
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-07 22:14:38 +0300

    ImageGalleryItem class => component

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,20 +1,30 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
-// import { Modal } from 'components/Modal';
 import css from './ImageGallery.module.css';
 export class ImageGallery extends Component {
- 
+  onGallery = (event, largeImageURL) => {
+    event.target && this.props.setLargeImageURL(largeImageURL);
+    this.props.toggleModal();
+  };
   render() {
+    const { articles } = this.props;
     return (
-      <ul className={css.ImageGallery}>
-        <ImageGalleryItem
-          articles={this.props.articles}
-          toggleModal={this.props.toggleModal}
-          setLargeImageURL={this.props.setLargeImageURL}
-          clearLargeImageURL={this.props.clearLargeImageURL}
-        />
-      </ul>
+      <>
+        {articles.length > 0 &&
+          articles.map(({ id, tags, webformatURL, largeImageURL }) => (
+            <ul
+              className={css.ImageGallery}
+              onClick={event => this.onGallery(event, largeImageURL)}
+            >
+              <ImageGalleryItem
+                id={id}
+                webformatURL={webformatURL}
+                tags={tags}
+              />
+            </ul>
+          ))}
+      </>
     );
   }
 }
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,34 +1,22 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import urlPropType from 'url-prop-type';
+import React from 'react';
 import css from './ImageGalleryItem.module.css';
-export class ImageGalleryItem extends Component {
-  onGallery = (largeImageURL) => {
-    this.props.setLargeImageURL(largeImageURL);
-    this.props.toggleModal();
-  };
-  render() {
-    return (
-      <>
-        {this.props.articles.length > 0 &&
-          this.props.articles.map(
-            ({ id, tags, webformatURL, largeImageURL }) => (
-              <li
-                key={id}
-                className={css.ImageGalleryItem}
-                onClick={() => this.onGallery( largeImageURL)}
-              >
-                <img
-                  src={webformatURL}
-                  alt={tags}
-                  className={css['ImageGalleryItem-image']}
-                />
-              </li>
-            )
-          )}
-      </>
-    );
-  }
-}
+export const ImageGalleryItem = ({ id, webformatURL, tags }) => {
+  return (
+    <>
+      <li key={id} className={css.ImageGalleryItem}>
+        <img
+          src={webformatURL}
+          alt={tags}
+          className={css['ImageGalleryItem-image']}
+        />
+      </li>
+    </>
+  );
+};
 ImageGalleryItem.propTypes = {
-  articles: PropTypes.arrayOf(PropTypes.object).isRequired,
+  id: PropTypes.number.isRequired,
+  webformatURL: urlPropType.isRequired,
+  tags: PropTypes.string.isRequired,
 };
