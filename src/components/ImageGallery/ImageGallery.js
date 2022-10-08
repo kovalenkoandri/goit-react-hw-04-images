ebf9b4ab6e29e40cf24aafcb02da7e54c7207830
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-10-08 21:36:01 +0300

    ImageGallery & ImageGalleryItem in 1 file

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,29 +1,36 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { ImageGalleryItem } from 'components/ImageGalleryItem';
+// import { ImageGalleryItem } from 'components/ImageGalleryItem';
+import cssItem from 'components/ImageGalleryItem/ImageGalleryItem.module.css';
 import css from './ImageGallery.module.css';
 export class ImageGallery extends Component {
   onGallery = (event, largeImageURL) => {
-    event.target && this.props.setLargeImageURL(largeImageURL);
+    event.currentTarget && this.props.setLargeImageURL(largeImageURL);
     this.props.toggleModal();
   };
   render() {
     const { articles } = this.props;
     return (
       <>
-        {articles.length > 0 &&
-          articles.map(({ id, tags, webformatURL, largeImageURL }) => (
-            <ul
-              className={css.ImageGallery}
-              onClick={event => this.onGallery(event, largeImageURL)}
-            >
-              <ImageGalleryItem
-                id={id}
-                webformatURL={webformatURL}
-                tags={tags}
-              />
-            </ul>
-          ))}
+        <ul className={css.ImageGallery}>
+          {articles.length > 0 &&
+            articles.map(({ id, tags, webformatURL, largeImageURL }) => {
+              console.log(id);
+              return (
+                <li
+                  key={id.toString()}
+                  className={cssItem.ImageGalleryItem}
+                  onClick={event => this.onGallery(event, largeImageURL)}
+                >
+                  <img
+                    src={webformatURL}
+                    alt={tags}
+                    className={cssItem['ImageGalleryItem-image']}
+                  />
+                </li>
+              );
+            })}
+        </ul>
       </>
     );
   }
